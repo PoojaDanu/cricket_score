a2c33d6865ef4d1902c6acd75b703b3a68b37ba1
Virat <vbaghel@aistrike.com>
2024-03-28 17:35:16 +0530

    Feat: Kafka producer added

--- cric/cric/kafka_producer.py
+++ cric/cric/kafka_producer.py
@@ -1,12 +1,26 @@
-from aiokafka import AIOKafkaProducer
+from confluent_kafka import Producer
 
-class MessageProducer:
-    __message_producer:AIOKafkaProducer = None
+ssl_config = {
+    "ca_location": "../kafka_files/ca.pem",
+    "cert_location": "../kafka_files/service.cert",
+    "key_location": "../kafka_files/service.key",
+}
 
-    def setup_producer(self) -> AIOKafkaProducer:
-        kafka_producer = AIOKafkaProducer(bootstrap_servers="kafka-1ddd1fb7-adityacha703-e344.a.aivencloud.com:18505")
-        MessageProducer.__message_producer = kafka_producer
-        return self.__message_producer;
+class KafkaProducer:
+    def __init__(self):
+        self.producer = Producer({
+            'bootstrap.servers': "kafka-1ddd1fb7-adityacha703-e344.a.aivencloud.com:18505",
+            'security.protocol': 'ssl',
+            'ssl.ca.location': ssl_config['ca_location'],
+            'ssl.certificate.location': ssl_config['cert_location'],
+            'ssl.key.location': ssl_config['key_location']
+        })
 
-    def send(self, message , topic):
-        self.__kafka_producer.send(topic ,message)
+    def produce_message(self, topic, message):
+        self.producer.produce(topic=topic, value=message)
+        self.producer.flush()
+
+    def close(self):
+        self.producer.flush()
+        self.producer.close()
+
--- cric/cric/view.py
+++ cric/cric/view.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from .kafka_producer import KafkaProducer
 
-def homepage(request):
-    
+async def homepage(request):
+    __kafka_produce = KafkaProducer()
+    __kafka_produce.produce_message("score","himanshu")
     return HttpResponse('homepage')
